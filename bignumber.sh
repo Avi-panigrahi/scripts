842cb5a71b808157bce1efd5c211e55e2bbb41d0
Avi <apee.panigrahi58@gmail.com>
2023-02-13 23:44:39 +0530

    new record

--- bignumber.sh
+++ bignumber.sh
@@ -3,15 +3,15 @@ printf "enter A:"
 read A
 printf "enter B:"
 read B
-if[ $A -gt $B ]
+if [ $A -gt $B ]
 then
    echo "$A is bigger then $B"
 fi
-if[ $B -gt $A ]
+if [ $B -gt $A ]
 then
    echo "$B is bigger then $A"
 fi
-if[ $A -eq $B ]
+if [ $A -eq $B ]
 then
    echo "both are equal"
 fi

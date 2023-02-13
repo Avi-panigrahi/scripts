edfc4f6cfa6fd4d200a5f9ba0c3542046aca86b9
Avi <apee.panigrahi58@gmail.com>
2023-02-13 23:16:48 +0530

    new file created to find big number

---
+++ bignumber.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+printf "enter A:"
+read A
+printf "enter B:"
+read B
+if[ $A gt $B ]
+then
+    echo"$A is bigger then $B"
+fi
+if[ $B -gt $A ]
+then
+    echo"$B is bigger then $A"
+fi
+if[ $A -eq $B ]
+then
+    echo"both are equal"
+fi
